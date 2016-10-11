2dd9273c154d6b4add24bb2fbc5c98ded3d013af
rodorad <rene.o.dorado@intel.com>
2016-10-11 16:38:06 -0700

    create model scoring package

--- package.sh
+++ package.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+MODULE=model-scoring-$VERSION.$POST_TAG$BUILD_NUMBER
+
+
+pushd target
+
+    mkdir -p $MODULE/bin
+
+    cp -Rv lib $MODULE/
+
+    ls -la $MODULE
+
+    cp model-scoring*.jar $MODULE/
+
+    cp ../bin/model-scoring.sh $MODULE/bin/
+
+
+    zip  -r $MODULE.zip $MODULE
+
+popd
+
+
+
+
+
+
+
