7fcbf0ccb4d89ef5759dc131e00ad7ef68d2e152
Anjali Sood <anjali.sood@intel.com>
2016-10-13 13:27:01 -0700

    added license.sh and added missing license headers

--- license.sh
+++ license.sh
@@ -16,32 +16,4 @@
 #
 
 
-MODULE=model-scoring-$VERSION.$POST_TAG$BUILD_NUMBER
-
-
-pushd target
-
-    mkdir -p $MODULE/bin
-    mkdir -p $MODULE/conf
-
-    cp -Rv lib $MODULE/
-
-    ls -la $MODULE
-
-    cp model-scoring*.jar $MODULE/
-
-    cp ../bin/model-scoring.sh $MODULE/bin/
-    cp ../conf/application.conf.cf $MODULE/conf/application.conf
-    cp ../manifest.yml.tpl $MODULE/manifest.yml
-
-
-    zip  -r $MODULE.zip $MODULE
-
-popd
-
-
-
-
-
-
-
+ mvn com.mycila:license-maven-plugin:2.3:$1
