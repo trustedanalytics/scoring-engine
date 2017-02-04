1e667a0ca06afe97e03b0415dcf08fa80ae75880
jitendra42 <jitendra.patil@intel.com>
2017-02-03 16:06:31 -0800

    adding s3 support for scoring engine

--- model-scoring-core/src/main/scala/org/trustedanalytics/scoring/ScoringEngineHelper.scala
+++ model-scoring-core/src/main/scala/org/trustedanalytics/scoring/ScoringEngineHelper.scala
@@ -17,14 +17,20 @@
 package org.trustedanalytics.scoring
 
 import java.io.File
+import java.net.URI
 import java.util.{ArrayList => JArrayList}
 
+import com.typesafe.config.ConfigFactory
+import org.apache.commons.io.FileUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
 import org.slf4j.LoggerFactory
 import org.trustedanalytics.model.archive.format.ModelArchiveFormat
 import org.trustedanalytics.scoring.interfaces.Model
 
 object ScoringEngineHelper {
   private val logger = LoggerFactory.getLogger(this.getClass)
+  val config = ConfigFactory.load(this.getClass.getClassLoader)
 
   /**
     *
@@ -39,6 +45,21 @@ object ScoringEngineHelper {
       model.output().deep == revisedModel.output().deep
   }
 
+  private def getAWSConfig(): Configuration = {
+    val proxyHost = config.getString("trustedanalytics.aws.fs.s3a.proxy.host")
+    val proxyPort  = config.getString("trustedanalytics.aws.fs.s3a.proxy.port")
+    val accessKey = config.getString("trustedanalytics.aws.fs.s3a.access.key")
+    val secretKey = config.getString("trustedanalytics.aws.fs.s3a.secret.key")
+
+    val cfg =  new Configuration()
+    if(proxyHost != "" && proxyPort != "") {
+      cfg.set("fs.s3a.proxy.host",proxyHost)
+      cfg.set("fs.s3a.proxy.port",proxyPort)
+    }
+    if(accessKey != "") cfg.set("fs.s3a.access.key",accessKey) else throw new Exception("Configuration do not have AWS access key")
+    if(secretKey != "") cfg.set("fs.s3a.secret.key",secretKey) else throw new Exception("Configuration do not have AWS secret key")
+    cfg
+  }
   def getModel(marFilePath: String): Model = {
     if (marFilePath != "") {
       try {
@@ -46,16 +67,21 @@ object ScoringEngineHelper {
         return ModelArchiveFormat.read(new File(marFilePath), this.getClass.getClassLoader, None)
       } catch {
         case e: Exception =>
-          try {
-            logger.info("Unale to load model from local filesystem, trying to load the model from HDFS")
-            tempMarFile = File.createTempFile("model", ".mar")
+          logger.info("Unale to load model from local filesystem, trying to load the model from HDFS")
+          var tempMarFile: File = null
+          tempMarFile = File.createTempFile("model", ".mar")
+          try
+          {
+            val cfg = getAWSConfig()
+            val hdfsFileSystem = org.apache.hadoop.fs.FileSystem.get(new URI(marFilePath), cfg)
             hdfsFileSystem.copyToLocalFile(false, new Path(marFilePath), new Path(tempMarFile.getAbsolutePath))
             val hdfsMarFilePath = tempMarFile.getAbsolutePath
             sys.addShutdownHook(FileUtils.deleteQuietly(tempMarFile)) // Delete temporary directory on exit
             return ModelArchiveFormat.read(new File(hdfsMarFilePath), this.getClass.getClassLoader, None)
           } catch {
             case e: Exception =>
-              logger.info("Unale to load model from HDFS...")
+              logger.info("Unale to load model from HDFS...\n"+e.getMessage)
+              logger.info("\n"+e.getStackTraceString)
               return null
           } finally {
             FileUtils.deleteQuietly(tempMarFile)
